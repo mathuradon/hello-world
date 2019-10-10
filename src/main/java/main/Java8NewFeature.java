3b9f5126b8a3f9b80029a375619f26e671453b47
mathuradon <mathu.dahal@hotmail.com>
2019-10-10 00:42:18 -0500

    second Commit

---
+++ src/main/java/main/Java8NewFeature.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Java8NewFeature {
+    public static void main(String[] args) {
+        List<Human> humans = new ArrayList<>();
+        humans.add(new Human("Ramesh", 24));
+        humans.add(new Human("Bhim", 21));
+        humans.add(new Human("Jeevan", 32));
+        humans.add(new Human("Ghamesh", 35));
+        humans.add(new Human("Hari", 25));
+
+
+        /**
+         * Before Java 8
+         */
+        Collections.sort(humans, new Comparator<Human>() {
+            @Override
+            public int compare(Human o1, Human o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+        for (Human human : humans) {
+            System.out.println(human.getName() + "  "+ human.getAge());
+
+        }
+
+        /**
+         * After Java 8
+         */
+        System.out.println("===============NOW IN JAVA 8=================");
+        humans.sort((Human h1, Human h2) -> h1.getName().compareTo(h2.getName()));
+        for (Human human : humans) {
+            System.out.println(human.getName() + "  "+ human.getAge());
+
+        }
+    }
+}
---
+++ src/main/java/main/StringManipulation.java
@@ -0,0 +1,45 @@
+package main;
+
+public class StringManipulation {
+
+    public static void main(String[] args) {
+        //String result = reverseString( "Hello");
+        //System.out.println(result);
+        //String response = stringReverse("hello");
+        //System.out.println(response);
+        String str = "Hello";
+        char[] strArray = str.toCharArray();
+        char[] charArray = {};
+
+        for(int i = strArray.length; i > 0; i--){
+            char temp = strArray[strArray.length -i];
+            
+            System.out.println(temp);
+
+        }
+
+
+    }
+
+    /*private static String stringReverse(String reversethis) {
+        String rev = "";
+        for(int i = 0; i <= reversethis.length() - 1; i++){
+            rev = rev + reversethis.charAt(reversethis.length() -1 - i);
+        }
+        return rev;
+    }
+
+    public static String reverseString(String str){
+        char[] charArray = str.toCharArray();
+        int half = charArray.length / 2;
+        int last = charArray.length - 1;
+
+        for(int i = 0; i < half/2; i++){
+            char temp = charArray[i];
+            charArray[i] = charArray[last -i];
+            charArray[last -i] = temp;
+        }
+        return new String(charArray);
+    }
+*/
+}
